2fa8c1cd63142f72f8b5a5076e7608d03322285f
Badal Raina <malhaar6566@hotmail.com>
2020-04-17 23:12:57 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://typewrite.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by Badal Raina',
+  subtitle: 'About engineering and web stuff.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-162724105-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Badal Raina',
+    photo: '/photo.jpg',
+    bio: '',
+    contacts: {
+      email: 'malhaar6566@hotmail.com',
+      facebook: 'malhaar6566',
+      telegram: 'malhaar6566',
+      twitter: 'malhaar6566',
+      github: 'malhaar6566',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'badalraina',
+      instagram: 'malha.ar',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
---
+++ content/pages/contacts.md
@@ -0,0 +1,7 @@
+---
+title: "Contact me"
+template: "page"
+socialImage: "/media/image-4.jpg"
+---
+
+[contact here](malhaar6566@hotmail.com)
